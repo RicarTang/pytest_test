e619d941db182bf1c340193b7e9f1d6644abfe00
RicarTang <357965604@qq.com>
2022-11-24 17:42:41 +0800

    验证driver书写方式

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 
 import pytest
@@ -6,5 +5,5 @@ import pytest
 import yaml
 
 if __name__ == '__main__':
-    pytest.main([ 'test_case/test_data.py','-sv','--alluredir','./report/report-pytest', '--clean-alluredir'])
+    pytest.main(['test_case/test_data.py', '--alluredir', './report/report-pytest', '--clean-alluredir'])
     os.system('allure generate ./report/report-pytest -o ./report/report-allure --clean')
